0d6d035fd0378f30b9e4310fc54e31b78cb40158
Santosh Mondal <santosh.ece06@gmail.com>
2021-05-18 19:03:00 +0530

    project 10 post mapping.

--- src/main/java/com/capgemini/controller/HelloWorldController.java
+++ src/main/java/com/capgemini/controller/HelloWorldController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,12 @@ public class HelloWorldController {
 		e.setMobile("1122334455");
 		
 		return new ResponseEntity<Employee>(e, HttpStatus.OK);
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+		
+		// logic to service/ repository
+		return new ResponseEntity<Employee>(employee, HttpStatus.CREATED);
 	}
 }
--- src/test/java/com/capgemini/controller/HelloWorldControllerTest.java
+++ src/test/java/com/capgemini/controller/HelloWorldControllerTest.java
@@ -2,13 +2,18 @@ package com.capgemini.controller;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.capgemini.entities.Employee;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @WebMvcTest(HelloWorldController.class)
@@ -31,6 +36,23 @@ class HelloWorldControllerTest {
 		mockMvc.perform(get("/api/hello-world/12"))
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.email").value("rohit@gmail.com"));
+	}
+	
+	
+	@Test
+	void createEmployeeTest() throws Exception {
+		
+		Employee employee = new Employee();
+		employee.setName("rahul");
+		employee.setEmail("rahul@gmail.com");
+		employee.setMobile("22334455");
+		
+		mockMvc.perform(post("/api/hello-world/")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(new ObjectMapper().writeValueAsBytes(employee)))
+				.andExpect(MockMvcResultMatchers.status().isCreated())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.email").value("rahul@gmail.com"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("rahul"));
 	}
 
 }
